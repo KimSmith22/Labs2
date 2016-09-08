68970add43f540d83286be01f1e372f3c09fb054
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-08 10:42:47 -0400

    variables

--- src/gcLabz2/Labs2AdventureGame.java
+++ src/gcLabz2/Labs2AdventureGame.java
@@ -1,10 +1,21 @@
 package gcLabz2;
 
+import java.util.Scanner;
+
 public class Labs2AdventureGame {
 
+	private static Scanner k;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//declare variables
+		String yourName;
+		String dragonName;
+		String choiceString;
+		String battleString;
+		String headInt;	
+		String weaponString;
+		String colorString;
+		
 	}
-
 }
